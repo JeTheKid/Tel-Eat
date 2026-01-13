b2f6dca1cd19ebb20968c0f749917830633a738b
JeTheKid <buburayam3927@gmail.com>
2026-01-13 14:52:06 +0700

    memperbaiki route logout dari POST menjadi ANY untuk menangani hosting redirect issue

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware(['auth'])->group(function () {
 
     // Logout
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::any('/logout', [AuthController::class, 'logout'])->name('logout');
 
     // Profile Mahasiswa
     Route::get('/profil', [ProfileController::class, 'index'])->name('profile.index');
